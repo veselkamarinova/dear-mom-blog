d63c155bd8b94cd3afe6716c8797b70fb69d5fc2
Pieter Frenssen <pieter@frenssen.be>
2025-08-23 12:40:35 +0200

    T1193: Create a subtheme for the Dear Mom blog.

---
+++ web/themes/dear_mom/js/dear-mom.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Dear mom behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.dearMom = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
